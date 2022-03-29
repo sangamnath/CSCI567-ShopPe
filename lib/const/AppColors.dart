108513a36b12c2806e9984d55400ffebf61620c9
Sangu Teli <sangamnatth777@gmail.com>
2022-03-29 00:59:02 -0700

    Login setup

---
+++ lib/const/AppColors.dart
@@ -0,0 +1,7 @@
+// ignore: file_names
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  // ignore: constant_identifier_names
+  static const Color deep_orange = Color(0xFFFF6B6B);
+}
---
+++ lib/ui/splash_screen.dart
@@ -0,0 +1,53 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shop_pe/const/AppColors.dart';
+import 'package:shop_pe/ui/login_screen.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.push(
+            context, CupertinoPageRoute(builder: (_) => LoginScreen())));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.deep_orange,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "E-Commerce",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 44.sp),
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+              CircularProgressIndicator(
+                color: Colors.white,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
